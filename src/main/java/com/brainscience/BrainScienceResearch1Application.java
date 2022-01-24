344e694b1da9ddfe57e03a302e891d97942bf9b7
renhai1 <1457481392@qq.com>
2022-01-24 09:51:57 +0800

    初始化仓库

---
+++ src/main/java/com/brainscience/BrainScienceResearch1Application.java
@@ -0,0 +1,13 @@
+package com.brainscience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainScienceResearch1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainScienceResearch1Application.class, args);
+    }
+
+}
